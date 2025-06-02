674c1f081ab5228713d6fdfee14b77dba568e5ea
Marcel Reig <marcelreig@gmail.com>
2025-06-02 19:08:57 +0000

    edit style

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-light text-center text-muted py-3 mt-5">
+    <footer className="bg-dark text-light text-center py-3 mt-5">
       <div className="container">
         <small>&copy; 2025 Netinetidesign</small>
       </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Jumbotron from "./Jumbotron";
-import Card from "./Card";
+
 import Cards from "./Cards";
 
 //create your first component
